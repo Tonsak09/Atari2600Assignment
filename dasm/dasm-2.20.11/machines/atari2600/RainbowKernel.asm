c48c4ab213c1a3b3aab3ebbcda944c1fc4e19476
Narai Risser <43816475+Tonsak09@users.noreply.github.com>
2024-09-16 17:14:14 -0400

    Final

--- dasm/dasm-2.20.11/machines/atari2600/RainbowKernel.asm
+++ dasm/dasm-2.20.11/machines/atari2600/RainbowKernel.asm
@@ -6,65 +6,42 @@
 Reset
 StartOfFrame
 	; Start of vertical blank processing
-	lda #0
-	sta VBLANK
-	lda #2
-	sta VSYNC
+	lda #0										; 2
+	sta VBLANK									; 3
+	lda #2										; 2
+	sta VSYNC									; 3
 	; 3 scanlines of VSYNCH signal...
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	lda #0
-	sta VSYNC
+	sta WSYNC									; 3
+	sta WSYNC									; 3
+	sta WSYNC									; 3
+	lda #0										; 2
+	sta VSYNC									; 3
 	; 37 scanlines of vertical blank...
-	ldx #0
-	stx $40
-loop inx
-	cpx #37
-	sta WSYNC
-	bne loop 
-
+	ldx #0										; 2
+	stx $40										; 3
+loopa inx
+	cpx #37										; 2
+	sta WSYNC									; 3
+	bne loopa									; 3
 
 	; 192 scanlines of picture...
-	ldx #0
+	ldy #50										; 2
 	REPEAT 192; scanlines
-		inx
-		stx COLUBK
-		sta WSYNC
+		;iny									; 2
+		sty COLUBK								; 3
+		
+		sta WSYNC								; 3
 	REPEND
-	lda #%01000010
-	sta VBLANK ; end of screen - enter blanking
+	lda #%01000010								; 2
+	sta VBLANK ; end of screen - enter blanking ; 3
 	; 30 scanlines of overscan...
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
-	sta WSYNC
+	ldx #0										; 2
+	stx $40										; 3
+loopb inx
+	cpx #37										; 2
+	sta WSYNC									; 3
+	bne loopb									; 3
+	
 	jmp StartOfFrame
 	ORG $FFFA
 	.word Reset ; NMI
